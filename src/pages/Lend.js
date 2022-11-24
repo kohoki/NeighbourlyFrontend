659ad911d8780e87000081de1f1a23d7cfc7e373
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-11-24 14:36:05 +0100

    Created form on Lend page

--- src/pages/Lend.js
+++ src/pages/Lend.js
@@ -1,7 +1,63 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const API_URL = "http://localhost:5005";
+
 function Lend () {
+    const [itemName, setItemName] = useState("");
+    const [image, setImage] = useState("");
+    const [description, setDescription] = useState("");
+    const [availability, setAvailability] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleItemName = (e) => setItemName(e.target.value);
+    const handleImage = (e) => setImage(e.target.value);
+    const handleDescription = (e) => setDescription(e.target.value);
+    const handleAvailability = (e) => setAvailability(e.target.value);
+
+    const handleCreateSubmit = (e) => {};
+
+
     return (
-        <div>
+        <div className="LendPage">
             <h1>Sharing is caring. What would you like to lend?</h1>
+            <form onSubmit={handleCreateSubmit}>
+        <label>Item name:</label>
+          <input 
+            type="text"
+            name="itemName"
+            value={itemName}
+            onChange={handleItemName}
+          />
+
+          <label>Select an image:</label>
+          <input 
+            type="file"
+            name="image"
+            value={image}
+            onChange={handleImage}
+          />
+   
+          <label>Description:</label>
+          <input 
+            type="text"
+            name="description"
+            value={description}
+            onChange={handleDescription}
+          />
+
+          <label>Availability of item:</label>
+          <input 
+            type="text"
+            name="availability"
+            value={availability}
+            onChange={handleAvailability}
+          />
+   
+          <button type="submit">Create item for lending</button>
+        </form>
         </div>
     )
 }
