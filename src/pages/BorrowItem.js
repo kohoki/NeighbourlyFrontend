1560b7ad8e1b6eed922b8b74817323060867379a
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-11-25 18:00:28 +0100

    Working on sending a message for borrowing item

--- src/pages/BorrowItem.js
+++ src/pages/BorrowItem.js
@@ -9,23 +9,38 @@ const API_URL = "http://localhost:5005";
 function BorrowItem (props) {
     const [lender, setLender] = useState();
     const [item, setItem] = useState();
+    const [message, setMessage] = useState();
     const itemId = useParams()
+    const [errorMessage, setErrorMessage] = useState(undefined);
+    const {user} = useContext(AuthContext)
+    const navigate = useNavigate();
 
 
     const fetchItemAndLender = async () => {
         let response = await axios.get(`${API_URL}/item/borrow/${itemId.itemId}`)
         setItem(response.data.item);
-        console.log(response.data.item)
-        //console.log(response.data.foundedItems);
         setLender(response.data.item.creator);
-        //console.log('Lender: ', lender);
     }
 
     useEffect(() => {
         fetchItemAndLender();
     }, []);
 
-    console.log('Lender: ', lender);
+    const handleMessage = (e) => setMessage(e.target.value);
+
+    const handleMessageSubmit = async (e) => {
+        try {
+            e.preventDefault();
+            const requestBody = {item: itemId.itemId, lender: lender._id, borrower: user._id, communication: [{message: message, userId: user._id}]};
+            console.log(requestBody)
+
+            const postMessage = await axios.post(`${API_URL}/messages`, requestBody);
+            navigate(`/profile/${user._id}`);
+        } catch (error) {
+            const errorDescription = error.response.data.message;
+            setErrorMessage(errorDescription);
+        }
+    }
     
 
 
@@ -45,7 +60,13 @@ function BorrowItem (props) {
             </div>
             <div className="messages">
                 <h2>Get in touch with the lender: </h2>
-                <input />
+                <form onSubmit={handleMessageSubmit}>
+                    <label> Write a message to the lender: </label>
+                      <input type="text" name="message" value={message} onChange={handleMessage}/>
+                      <button type="submit">Submit message</button> 
+                </form>
+
+                { errorMessage && <p className="error-message">{errorMessage}</p> }
             </div>
         </>
     ) : (
