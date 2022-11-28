feceaef9e95ece1e966017b6316647a9155cd55b
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-11-28 16:11:45 +0100

    Updated profile page for checking if addresses are defined

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -32,15 +32,18 @@ useEffect(() => {
 
             <div>
             <h2>Addresses</h2>
-            <h3>Main Address: </h3>
-            <div>
-            <ul>
-        <li className="ListItem">{address.nameOfAddress}</li>
-        <li className="ListItem">{address.number} {address.street}</li>
-        <li className="ListItem">{address.postalCode}</li>
-        <li className="ListItem">{address.city}</li>
-            </ul>
-            </div>
+            {address ? (
+                <>
+              <h3>Main Address: </h3>
+                <ul>
+                    <li className="ListItem">{address.nameOfAddress}</li>
+                    <li className="ListItem">{address.number} {address.street}</li>
+                    <li className="ListItem">{address.postalCode}</li>
+                    <li className="ListItem">{address.city}</li>
+                </ul> 
+                </>
+            ) : (<></>)}
+            
             <Link to={`/profile/${user._id}/address`}>Update Addresses</Link>
             </div>
            
