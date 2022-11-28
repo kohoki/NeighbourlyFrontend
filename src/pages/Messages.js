436bc271ff1aea73047f24bc28056ede6cacaa15
Ralf Bonnick <ralfbonnick@gmx.net>
2022-11-28 14:01:26 +0100

    start to create messages.js

--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,9 +1,33 @@
-function Messages () {
-    return (
+import { Link } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
+import { AuthContext } from "../context/auth.context";
+import axios from "axios";
+
+const API_URL = "http://localhost:5005";
+
+function Messages() {
+  const { user } = useContext(AuthContext);
+  const [messages, setMessages] = useState();
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      let response = await axios.post(`${API_URL}/messages/${user._id}`);
+      setMessages(response.data.messages);
+    };
+    fetchMessages();
+  }, []);
+
+  return (
+    <div>
+      {messages.map((message) => (
         <div>
-            <h1>Your Messages</h1>
+          <ul className="ListItem">
+            <li key={message._id}>{message.borrower}</li>
+          </ul>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default Messages;
