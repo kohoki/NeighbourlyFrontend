45a199ef60dd3bff8214c8ec9ebf25e2cf782182
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-11-29 11:26:14 +0100

    Changed DeleteItems comp

--- src/components/DeleteItem.js
+++ src/components/DeleteItem.js
@@ -3,16 +3,16 @@ import axios from "axios";
 const API_URL = "http://localhost:5005";
 
 function DeleteItem({itemId, setItems, userId}) {
-const handleDelete = async() => {
-await axios.delete(`${API_URL}/item/${userId}/delete/${itemId}`)
+const handleDelete = async(e) => {
+e.preventDefault();
+await axios.delete(`${API_URL}/item/${userId}/delete/${itemId}`);
 let response = await axios.get(`${API_URL}/item/${userId}`)
-console.log(response.data)
 setItems(response.data.foundedItems)
 } 
 
 return (
 <>
-<button className="Button" type="submit" onClick={handleDelete}>Remove Item</button>
+<button className="Button" type="button" onClick={handleDelete}>Remove Item</button>
 </>
     )
 }
