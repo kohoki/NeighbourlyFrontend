99faadaf332bb10b6b3db9ffed046e3e669633ea
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-11-29 17:02:42 +0100

    On page displaying all currently borrowed items

--- src/pages/BorrowedItems.js
+++ src/pages/BorrowedItems.js
@@ -1,6 +1,39 @@
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
+import { AuthContext } from "../context/auth.context";
+
+const API_URL = "http://localhost:5005"
+
 function BorrowedItems () {
-    return (
-        <h1>These items are currently borrowed:</h1>
+    const { user } = useContext(AuthContext);
+    const [items, setItems] = useState();
+
+    useEffect(() => {
+        const fetchItems = async () => {
+            let response = await axios.get(`${API_URL}/item/${user._id}/borrowed`)
+            setItems(response.data.foundedItems)
+        }
+        fetchItems();
+    }, []);
+    
+    return items? (
+        <div>
+          <h1>These items are currently borrowed:</h1>
+          {items.map(item => (
+                <div key={item._id}>
+                <p>Item name: {item.itemName}</p>
+                <p>Image: <img src={item.image} alt="item-pic"/></p>
+                <p>Description: {item.description}</p>
+                <p>Availability: {item.availability}</p>
+                <p>Lender: {item.creator.username}</p>
+                </div>
+            ))}
+            <Link className="Link" to="/borrow">Borrow Another Item</Link>
+        </div>
+    )
+    : (
+        <h1>Loading...</h1>
     )
 }
 
